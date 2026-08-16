c90c56f95c21e789a20092c1079540eca40f540e
bloggerenka <121873699+bloggerenka@users.noreply.github.com>
2026-08-16 16:21:53 +0700

    Update arjuna.js

--- arjuna.js
+++ arjuna.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://oggoel.diirtyflirtclub.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=181648&s2=2278498&s3=Melisa"; } LoadAd();
+function LoadAd() { parent.location.href="https://oggoel.desiirefinder.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=181648&s2=2250616&s3=Arjuna"; } LoadAd();
